d7215a55e27142a50f37d8e093960b35826acf01
nicolasmcort <evmunoz@unal.edu.co>
2025-03-04 12:36:58 -0500

    Add files via upload

---
+++ POO_flet/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+image_firefox = ImageElement(
+    #     src="C:/Users/nicol/Downloads/firefox-57-70.svg",  # URL de la imagen
+    #     left=1050,
+    #     top=35,
+    #     width=50,  # Ancho
+    #     height=50,  # Alto
+    #     fit=ft.ImageFit.CONTAIN,  # Ajuste de la imagen
+    #     page=page,  # Referencia a la página
+    # )
+    # image_edge = ImageElement(
+    #     src="C:/Users/nicol/Downloads/microsoft-edge.svg",  # URL de la imagen
+    #     left=1130,
+    #     top=35,
+    #     width=50,  # Ancho
+    #     height=50,  # Alto
+    #     fit=ft.ImageFit.CONTAIN,  # Ajuste de la imagen
+    #     page=page,  # Referencia a la página
+    # )
