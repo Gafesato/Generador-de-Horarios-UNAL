155367aaee144b009dc18eb8c61d41ed03c1d96b
eljuanessoy <141857576+eljuanessoy@users.noreply.github.com>
2025-03-04 21:31:21 -0500

    Update switch.py

--- generator/interface/switch.py
+++ generator/interface/switch.py
@@ -6,14 +6,14 @@ from generator.interface.widget import Widget
 # Widget Switch 
 class Switch(Widget):
     def __init__(self, value=False, left=0, top=0):
-        super().__init__("", left, top)  # "" porque no necesita una etiqueta
-        self.state = value  # Estado inicial del Switch (True o False)
+        super().__init__("", left, top)  # "" because it doesn't need a tag
+        self.state = value  # Initial state of the switch (True o False)
         self.switch = ft.Switch(value=value, on_change=self.toggle_state)
 
     def toggle_state(self, e):
         """Actualiza el estado del Switch."""
         self.state = self.switch.value
-        #print(f"Switch cambiado: {self.state}")  # Para verificar en la consola
+        #print(f"Switch cambiado: {self.state}")  # To check in the console
 
     def create(self):
         return ft.Container(content=self.switch, left=self.left, top=self.top)
