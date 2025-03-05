c54a02ac3fc7da58e07b0f7d3b6e89f2a20613f1
eljuanessoy <141857576+eljuanessoy@users.noreply.github.com>
2025-03-04 21:23:14 -0500

    Update checkbox.py

--- generator/interface/checkbox.py
+++ generator/interface/checkbox.py
@@ -7,14 +7,13 @@ from generator.interface.widget import Widget
 class CheckBox(Widget):
     def __init__(self, label, left=0, top=0):
         super().__init__(label, left, top)
-        self.state = False  # Estado inicial del CheckBox (True o False)
+        self.state = False  # Initial state of the CheckBox (True o False)
         self.checkbox = ft.Checkbox(label=label, on_change=self.toggle_var)
 
     def toggle_var(self, e):
         """Actualiza el estado del CheckBox."""
         self.state = self.checkbox.value
-        #print(f"CheckBox cambiado: {self.state}")  # Para verificar en la consola
+        #print(f"CheckBox cambiado: {self.state}")  # To check in the console
 
     def create(self):
         return ft.Container(content=self.checkbox, left=self.left, top=self.top)
-
