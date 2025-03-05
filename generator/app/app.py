03b722bbb243c679bea3bd56911224b36c698ff8
eljuanessoy <141857576+eljuanessoy@users.noreply.github.com>
2025-03-04 21:38:19 -0500

    Update app.py

--- generator/app/app.py
+++ generator/app/app.py
@@ -20,10 +20,10 @@ from generator.app.user import User
 
 
 def main():
-    # Crear una instancia de User con valores por defecto
+    # Create a User instance with default values
     usuario = User()
 
-    # Mostrar los datos del usuario
+    # Display user data
     print("\nDatos del usuario (valores por defecto):")
     print(f"Nivel de estudios: {usuario.nivel_de_estudios}")
     print(f"Sede: {usuario.sede}")
@@ -34,38 +34,38 @@ def main():
     print(f"Pesos: {usuario.pesos}")
     print(f"Límite de horarios a generar: {usuario.limit}")
 
-    # Crear una instancia de SubjectScraper para extraer las materias
+    # Create a SubjectScraper instance to extract the subjects
     scraper = SubjectScraper(usuario.navegador, usuario.nivel_de_estudios, usuario.sede, usuario.facultad,
                               usuario.plan_de_estudios, ["2016707", "2016699", "2016375"])
     scraper.run()
 
-    # Obtener las materias extraídas
+    # Obtaining the extracted subjects
     materias_extraidas = scraper.subjects
 
-    # Solicitar al usuario que ingrese sus grupos favoritos
-    materias_unicas = list(set(subject.name for subject in materias_extraidas))  # Nombres únicos de materias
+    # Ask the user to enter their favorite groups
+    materias_unicas = list(set(subject.name for subject in materias_extraidas))  # Unique subject names
     usuario.ingresar_grupos_favoritos(materias_unicas)
 
-    # Calcular la preferencia de cada materia en función de las preferencias del usuario
+    # Calculate the preference of each subject based on user preferences
     for subject in materias_extraidas:
         subject.preference = PreferenceCalculator.calcular_preferencia(
             subject, usuario.preferencias_generales, usuario.pesos, usuario.grupos_favoritos
         )
 
-    # Organizar las materias en una lista de listas (una lista por materia)
+    # Organize subjects into a list of lists (one list per subject)
     materias_organizadas = {}
     for subject in materias_extraidas:
         if subject.name not in materias_organizadas:
             materias_organizadas[subject.name] = []
         materias_organizadas[subject.name].append(subject)
 
-    # Convertir el diccionario a una lista de listas
+    # Convert dictionary to a list of lists
     subjects = list(materias_organizadas.values())
 
-    # Generar combinaciones válidas usando el heap
+    # Generate valid combinations using the heap
     valid_schedules = CombinationGenerator.generate_combinations(subjects, usuario.limit)
 
-    # Mostrar los horarios generados
+    # Show generated schedules
     print("\nHorarios válidos generados:")
     for i, schedule in enumerate(valid_schedules, start=1):
         print(f"\nHorario {i}:")
