9201c3efc8c7b60491d69f719185177304aa2fe7
eljuanessoy <141857576+eljuanessoy@users.noreply.github.com>
2025-03-04 21:38:54 -0500

    Update browser_factory.py

--- generator/app/browser_factory.py
+++ generator/app/browser_factory.py
@@ -8,15 +8,15 @@ class BrowserFactory:
     def get_browser(browser_name):
         if browser_name.lower() == "chrome":
             options = webdriver.ChromeOptions()
-            options.add_argument("--headless")  # Modo headless para Chrome
+            options.add_argument("--headless")  # Headless mode for Chrome
             return webdriver.Chrome(options=options)
         elif browser_name.lower() == "edge":
             options = webdriver.EdgeOptions()
-            options.add_argument("--headless")  # Modo headless para Edge
+            options.add_argument("--headless")  # Headless mode for Edge
             return webdriver.Edge(options=options)
         elif browser_name.lower() == "firefox":
             options = webdriver.FirefoxOptions()
-            options.add_argument("--headless")  # Modo headless para Firefox
+            options.add_argument("--headless")  # Headless mode for Firefox
             return webdriver.Firefox(options=options)
         else:
             raise ValueError(f"Navegador no soportado: {browser_name}")
