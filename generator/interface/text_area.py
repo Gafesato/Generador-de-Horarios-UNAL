7d563484ca763988478960e881f3a297820cc107
eljuanessoy <141857576+eljuanessoy@users.noreply.github.com>
2025-03-04 21:31:59 -0500

    Update text_area.py

--- generator/interface/text_area.py
+++ generator/interface/text_area.py
@@ -7,7 +7,7 @@ from generator.interface.widget import Widget
 class TextArea(Widget):
     def __init__(self, left=0, top=0, hint_area="Text Area", on_change=None):
         super().__init__("", left, top)
-        self.value = ""  # Para almacenar el texto ingresado
+        self.value = ""  #To store the entered text
         self.on_change = on_change
 
         self.text_area = ft.TextField(
@@ -15,13 +15,13 @@ class TextArea(Widget):
             label=hint_area,
             height=100,
             on_change=self.on_change,
-            on_blur=self.update_text  # Actualizar el valor al perder foco
+            on_blur=self.update_text  # Update value on focus loss
         )
 
     def update_text(self, e):
         """Actualiza el valor del TextArea cuando pierde el foco."""
         self.value = e.control.value
-        #print(f"Texto actualizado: {self.value}")  # Para depuración
+        #print(f"Texto actualizado: {self.value}")  # For debugging
 
     def create(self):
         return ft.Container(content=self.text_area, left=self.left, top=self.top)
